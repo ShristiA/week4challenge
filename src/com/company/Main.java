834683aeae09971059821891bf38c1457bdc45d0
ShristiA <shristiamatya3@gmail.com>
2018-10-12 11:37:43 -0400

    final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,11 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-
-
-
-
         String userAns = "Y";
 
         while(userAns.equalsIgnoreCase("Y")){
